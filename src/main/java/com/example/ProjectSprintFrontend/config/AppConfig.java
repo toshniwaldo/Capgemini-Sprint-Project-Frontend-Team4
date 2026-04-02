66246f81618f43a5b437236dd13ec9bc6fa24e73
jatin-ukey21 <jatinukey1@gmail.com>
2026-04-02 09:43:03 +0530

    Corrected the import of ObjectMapper

--- src/main/java/com/example/ProjectSprintFrontend/config/AppConfig.java
+++ src/main/java/com/example/ProjectSprintFrontend/config/AppConfig.java
@@ -3,7 +3,7 @@ package com.example.ProjectSprintFrontend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class AppConfig {
